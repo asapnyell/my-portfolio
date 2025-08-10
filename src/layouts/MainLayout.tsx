d9d50a739c5303bb57a850d2eace39780d9e5509
nyel the runner <149019962+asapnyell@users.noreply.github.com>
2025-08-10 08:39:46 -0300

    alteração de cor

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -9,7 +9,7 @@ type MainLayoutProps = {
 
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-indigo-900 via-purple-900 to-black text-white font-sans">
+    <div className="min-h-screen bg-gradient-to-br from-indigo-950 via-purple-900 to-black text-white font-sans">
       <Header />
       <div className="pt-20">{/* Espaço para o header fixo */}
         {children}
