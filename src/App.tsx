2a6b1433ebd38a2dd40095acd192667839dc85b2
nyel the runner <149019962+asapnyell@users.noreply.github.com>
2025-08-23 15:01:17 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-
-
 import MainLayout from "./layouts/MainLayout";
 import PresentationSection from "./components/presentationSection/PresentationSection";
 import Skills from "./components/skills/Skills";
 import React, { useRef, useEffect, useState } from "react";
 import Education from "./components/education/Education";
 import Projects from "./components/projects/Projects";
-import Services from "./components/services/Services";
+
 import Footer from "./components/Footer/Footer";
 import Contact from "./components/contact/Contact";
 
+import { Routes, Route } from "react-router-dom";
+import {ProjectsPage} from "./pages/projects";
 
 function FadeInOnScroll({ children, className = "", style = {}, padding = "" }: { children: React.ReactNode; className?: string; style?: React.CSSProperties; padding?: string }) {
   const ref = useRef<HTMLDivElement>(null);
@@ -41,30 +41,34 @@ function FadeInOnScroll({ children, className = "", style = {}, padding = "" }: 
 
 function App() {
   return (
-    <MainLayout>
-      <div className="flex flex-col gap-0">
-        <FadeInOnScroll padding="pb-4">
-          <div id="home">
-            <PresentationSection />
+    <Routes>
+      <Route path="/" element={
+        <MainLayout>
+          <div className="flex flex-col gap-0">
+            <FadeInOnScroll padding="pb-4">
+              <div id="home">
+                <PresentationSection />
+              </div>
+            </FadeInOnScroll>
+            <FadeInOnScroll padding="pt-10 pb-10">
+              <div id="skills">
+                <Skills />
+              </div>
+            </FadeInOnScroll>
+            <div id="projects">
+              <Projects/>
+            </div>
+            <div id="services">
+              
+            </div>
+            <Education/>
+            <Contact/>
+            <Footer/>
           </div>
-        </FadeInOnScroll>
-        <FadeInOnScroll padding="pt-10 pb-10">
-          <div id="skills">
-            <Skills />
-          </div>
-        </FadeInOnScroll>
-        <div id="projects">
-          <Projects/>
-        </div>
-        <div id="services">
-          <Services/>
-        </div>
-        <Education/>
-        <Contact/>
-        <Footer/>
-        
-      </div>
-    </MainLayout>
+        </MainLayout>
+      } />
+      <Route path="/projects" element={<ProjectsPage />} />
+    </Routes>
   );
 }
 
