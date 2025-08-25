16114e63b93f2d1d5608b38ebe253747aa45953c
ASAPNYEL <danyelhenrique941@gmail.com>
2025-08-25 10:46:40 -0300

    arrumando links

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -15,7 +15,7 @@ const Contacts: React.FC = () => {
       </a>
 
        <a
-        href="https://www.linkedin.com/in/asapnyell/"
+        href="https://www.linkedin.com/in/danyel-henrique-0880711a2"
         target="_blank"
         rel="noopener noreferrer"
         className="bg-indigo-500 hover:bg-indigo-700 transition-colors duration-200 text-white font-semibold py-2 px-6 rounded-lg shadow focus:outline-none focus:ring-2 focus:ring-indigo-400 text-base"
