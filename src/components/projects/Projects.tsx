79aa649c9485a346d5abd18f9deaf10672378e43
ASAPNYEL <danyelhenrique941@gmail.com>
2025-09-02 15:55:08 -0300

    Update Projects.tsx

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -8,20 +8,16 @@ const Projects: React.FC = () => {
     <section id="projects" className="w-full py-12 flex flex-col items-center animate-fade-in">
       <button
         onClick={() => setIsOpen(!isOpen)}
-        className="flex items-center space-x-3 text-3xl md:text-4xl font-bold mb-8 cursor-pointer bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 text-transparent bg-clip-text select-none"
+        className="flex items-center space-x-3 text-3xl md:text-4xl font-bold mb-8 cursor-pointer bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 text-transparent bg-clip-text select-none animate-pulse"
         aria-expanded={isOpen}
         aria-controls="projects-content"
+        
       >
-        <span>{isOpen ? "📂" : "📁"}</span>
-        <span>Meus Projetos</span>
+        <Link to="/ProjectsPage">
+          <span>{isOpen ? "📂" : "📁"}</span>
+          <span>Meus Projetos </span>
+        </Link>
       </button>
-
-      <Link
-        to="/ProjectsPage"
-        className="bg-white/10 backdrop-blur-md border border-white/30 rounded-xl px-6 py-2 text-white font-semibold shadow-lg hover:bg-white/30 transition animate-pulse"
-      >
-        Ver todos os projetos...
-      </Link>
     </section>
   );
 };
